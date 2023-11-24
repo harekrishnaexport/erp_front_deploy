7e9a2b8674dd5e60bfed02805e2691baffd06170
darpen dhameliya <dkdhameliya24@gmail.com>
2023-11-24 07:27:50 +0530

    24-11-home

--- src/component/Axios.js
+++ src/component/Axios.js
@@ -1,22 +1,22 @@
 import axios from "axios";
-const instance = axios.create({ baseURL: "http://192.168.252.99:5000/api" });
-// const instance = axios.create({ baseURL: "https://hkiepl.onrender.com/api" });
+// const instance = axios.create({ baseURL: "http://192.168.252.99:5000/api" });
+const instance = axios.create({ baseURL: "https://hkiepl.onrender.com/api" });
 export default instance;
 
 
-export const api = axios.create({
-  baseURL: "http://192.168.252.99:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-  },
-});
-
-
 // export const api = axios.create({
-//   baseURL: "https://hkiepl.onrender.com/api",
+//   baseURL: "http://192.168.252.99:5000/api",
 //   headers: {
 //     "Content-Type": "application/json",
 //     "Access-Control-Allow-Origin": "*",
 //   },
 // });
+
+
+export const api = axios.create({
+  baseURL: "https://hkiepl.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
